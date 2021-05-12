eb53046eb18bb13f4d470e4baabab43b62096416
Audronf <federicofasitella@gmail.com>
2021-05-12 12:46:12 -0300

    Saving PDF

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import os
 import requests
+import img2pdf
+import glob
 from lxml import html
 
+def save_pdf(pdf_name, path):
+    images_folder = glob.glob(path + '*.png')
+    with open(path + pdf_name + '.pdf', 'wb') as pdf:
+        pdf.write(img2pdf.convert(images_folder))
+    # Remove every .png image
+    for img in images_folder:
+        os.remove(img)
+
+# Create base folder
 try:
     folder_name = input('Folder name: ')
     os.mkdir(folder_name)
 except FileExistsError:
     print('Folder already exists.')
 
-# Preset values in key=value format
+# Read values from .env file
 with open('.env', 'r') as env:
     for line in env.readlines():
         pair = line.split('=')
@@ -16,13 +27,21 @@ with open('.env', 'r') as env:
 
 chapters = os.environ['CHAPTERS']
 base_url = os.environ['URL']
-
+ 
 for i in range(int(chapters)):
     r = requests.get(base_url + str(i + 1))
     response = html.fromstring(r.text)
     images = response.xpath('//img')
-    for index, img in enumerate(images, start = 1):
-        with open('./' + folder_name + '/' + str(i +1) + '_' + str(index) + '.png', 'wb') as file:
-            download = requests.get(img.attrib['src'])
-            file.write(download.content)
+    try: 
+        chapter_folder = 'Chapter ' + str(i + 1)
+        os.mkdir('./' + folder_name + '/' + chapter_folder)
+    except FileExistsError:
+        print('Folder ' + chapter_folder + ' already exists.')
+    for index, img in enumerate(images, start = 0):
+        if index != 0:
+            with open('./' + folder_name + '/' + chapter_folder + '/' + str(index) + '.png', 'wb') as file:
+                download = requests.get(img.attrib['src'])
+                file.write(download.content)
+    save_pdf('Chapter' + str(i + 1), "./%s/%s/" %(folder_name, chapter_folder))
     print('Chapter ' + str(i + 1) + ' finished!')
+
